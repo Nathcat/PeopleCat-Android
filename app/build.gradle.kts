a1a45c1702c7f1f8f670167a9afd1bf22ed6e9c3
Nathcat <74734054+Nathcat@users.noreply.github.com>
2025-02-21 19:47:01 +0000

    Changed version code and name

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -11,8 +11,8 @@ android {
         applicationId = "com.nathcat.peoplecat_client_android"
         minSdk = 30
         targetSdk = 34
-        versionCode = 5
-        versionName = "0.0.4-alpha"
+        versionCode = 6
+        versionName = "0.1.0-alpha"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables {
